08911178b8e866c3538f0e82e754d8bc8c943d5a
AmelieUlrick <amelieulrick.info@gmail.com>
2020-12-14 15:41:29 +0100

    when dans MenuActivity.kt

--- app/src/main/java/com/example/lajan/Activity/MenuActivity.kt
+++ app/src/main/java/com/example/lajan/Activity/MenuActivity.kt
@@ -2,19 +2,48 @@ package com.example.lajan.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.lajan.Fragment.HomeFragment
 import com.example.lajan.Fragment.ProfilFragment
 import com.example.lajan.Fragment.CarteFragment
 import com.example.lajan.R
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-    }
 
-    val homeFragment = HomeFragment()
-    val profilFragment = ProfilFragment()
-    val CarteFragment = CarteFragment()
+        val homeFragment = HomeFragment()
+        val profilFragment = ProfilFragment()
+        val CarteFragment = CarteFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottomNav.setOnNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.navigation_home -> {
+                    makeCurrentFragment(homeFragment)
+                    //intent.getIntExtra("id",data2)
+                }
+                R.id.navigation_profil -> {
+                    makeCurrentFragment(profilFragment)
+                    //intent.getIntExtra("id",data2)
+                }
+                R.id.navigation_carte -> {
+                    makeCurrentFragment(CarteFragment)
+                    //intent.putExtra("id",data2)
+                }
+            }
+            true
+        }
+        }
+
+
+    private fun makeCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, fragment)
+            commit()
+
+        }
 
 }

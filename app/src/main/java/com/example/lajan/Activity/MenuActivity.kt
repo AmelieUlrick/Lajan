b7d1016cd2e7958a4592f1214bd6de2d962228ef
Ameliie <amelieulrick.info@gmail.com>
2020-12-16 06:33:46 +0100

    CUSTOM APP

--- app/src/main/java/com/example/lajan/Activity/MenuActivity.kt
+++ app/src/main/java/com/example/lajan/Activity/MenuActivity.kt
@@ -3,12 +3,14 @@ package com.example.lajan.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.example.lajan.Bdd.DatabaseHelper
 import com.example.lajan.Fragment.CarteFragment
-import com.example.lajan.Fragment.HomeFragment
 import com.example.lajan.Fragment.ProfilFragment
+import com.example.lajan.MainActivity
 import com.example.lajan.R
 import kotlinx.android.synthetic.main.activity_menu.*
 import java.lang.System.exit
@@ -19,49 +21,46 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        val homeFragment = HomeFragment()
         val profilFragment = ProfilFragment()
         val CarteFragment = CarteFragment()
-
 
 
         //Récupère les données envoyées
         val intent : Intent = intent
         val idUser = intent.getIntExtra("idUser", 0)
 
+        val db = DatabaseHelper(this)
+
+        val user = db.getAll(idUser)
+
+        for(element in user){
+            menu_nom.text = element.Nom
+            menu_prenom.text = element.Prenom
+        }
+
+
+
         bottomNav.setOnNavigationItemSelectedListener{
+
+
+            showHide()
 
             //Envoie l'id de l'utilisateur dans chaque fragment du menu
             intent.putExtra("idUser", idUser)
             when (it.itemId){
-                R.id.navigation_home -> {
-                    makeCurrentFragment(homeFragment)
-                }
                 R.id.navigation_profil -> {
                     makeCurrentFragment(profilFragment)
                 }
                 R.id.navigation_carte -> {
                     makeCurrentFragment(CarteFragment)
-
+                }
+                R.id.navigation_deconnexion ->{
+                    exit(0)
                 }
             }
             true
         }
 
-        imgcb.setOnClickListener(object : View.OnClickListener {
-
-            override fun onClick(v: View?) {
-                makeCurrentFragment(homeFragment)
-               bottomNav.selectedItemId = R.id.navigation_home
-            }
-        })
-
-        imgLog.setOnClickListener(object : View.OnClickListener {
-
-            override fun onClick(v: View?) {
-               exit(0)
-            }
-        })
 
         }
 
@@ -71,5 +70,9 @@ class MenuActivity : AppCompatActivity() {
             commit()
 
         }
+    private fun showHide(){
+        val accueil = findViewById<LinearLayout>(R.id.menu)
+        accueil.visibility = View.GONE
+    }
 
 }
